51874b1038678d5d8d5feafa29991af928f4fe24
madbizarre <andrew-cool@bk.ru>
2015-10-30 03:52:19 +0300

    add config, contact us

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,48 @@
-;(function(){
-    $('.btn').on('click', function(){
+;
+(function (){
+    var closeAlert = function (){
+            var alert = $('.alert-danger');
+            if (alert.length) {
+                alert.close();
+            }
+        },
+        ajaxSuccess = function (data){
+            var contact = $('#contact');
+            closeAlert();
+            contact.before('<div class="alert alert-success" role="alert"><strong>Ваша заявка отправлена!</strong></div>');
+            contact.css('display', 'none');
+            setTimeout(function (){
+                $('#contactModal').modal('hide');
+            }, 1000);
+        },
+        ajaxError = function (err){
+            var contact = $('#contact'),
+                alert = $('.alert-danger');
+            if (alert.length) {
+                alert.close();
+            }
+            contact.before('<div class="alert alert-danger" role="alert"><strong>Произошла ошибка! Пожалуйста, повторите попытку.</strong></div>');
+        };
+
+    $('.btn').on('click', function (){
+        closeAlert();
         $('#contactModal').modal('show');
-        console.log('click');
+    });
+    $('#contact').on('submit', function (e){
+        e.preventDefault();
+        closeAlert();
+        $.ajax({
+            url: '/mail',
+            method: 'post',
+            data: {
+                name: $('#inputName').val(),
+                email: $('#inputEmail').val(),
+                vk: $('#inputVk').val(),
+                tel: $('#inputTel').val(),
+                comment: $('#inputComment').val()
+            },
+            success: ajaxSuccess,
+            error: ajaxError
+        });
     });
 })();
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,28 @@
 var express = require('express');
 var router = express.Router();
 var transporter = require('../lib/nodemailer');
+var config = require('../config');
 
-//transporter.sendMail({
-//  from: 'Павлов Андрей <darkbizarre@yandex.ru>',
-//  to: 'andrew-cool@bk.ru',
-//  subject: `Заявка с сайта: ${req.hostname}`,
-//  text: 'Test'
-//}, function (err){
-//  if (err) return next(err);
-//  res.render('index', { title: 'Express' });
-//});
+router.get('/', function (req, res, next){
+    res.render('index');
+});
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.post('/mail', function (req, res, next){
+    let mail = config.get('mail'),
+        text = `Имя: ${req.body.name}
+Email: ${req.body.email || 'не указан'}
+Vk: ${req.body.vk || 'не указан'}
+Телефон: ${req.body.tel}
+Комментарий: ${req.body.comment}`;
+    transporter.sendMail({
+        from: `${mail.from} <${mail.user}>`,
+        to: mail.to,
+        subject: `Заявка с сайта: ${req.hostname}`,
+        text: text
+    }, function (err){
+        if (err) return next(err);
+        res.status(200).end();
+    });
 });
 
 module.exports = router;
